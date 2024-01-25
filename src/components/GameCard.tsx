29bf954897595519a6cace63323f926603b47016
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-01-25 17:12:08 +0300

    Displat platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
+import PlatformIconList from './PlatformIconLists';
 
 interface Props {
   game: Game;
@@ -10,6 +11,9 @@ const GameCard = ({ game }: Props) => {
     <Card boxShadow={'xl'} borderRadius={8} overflow={'hidden'}>
       <Image src={game.background_image} />
       <CardBody>
+        <PlatformIconList
+          platforms={game.parent_platforms.map(p => p.platform)}
+        />
         <Heading fontSize={'2xl'}>{game.name}</Heading>
       </CardBody>
     </Card>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import apiClient from '../services/api-client';
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchResponse {
