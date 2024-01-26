8665cdc0f0c946bb7553243c2d726a625fa02bae
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-01-26 11:53:07 +0300

    Handle games without an image

--- src/services/img-url.ts
+++ src/services/img-url.ts
@@ -1,4 +1,7 @@
+import noImagePlaceHolder from '../assets/no-image-placeholder.webp';
+
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return noImagePlaceHolder;
   const index = url.indexOf('media/') + 'media/'.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 };
