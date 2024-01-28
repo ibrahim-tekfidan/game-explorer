2c67949f6968a8f58f1c8b6c9c0a37d2a17e046d
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-01-28 13:12:13 +0300

    Remove autoplay from video

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -14,7 +14,7 @@ const GameTrailer = ({ gameId }: Props) => {
   const first = trailers?.results[0];
 
   return first ? (
-    <video src={first?.data['480']} poster={first?.preview} controls autoPlay />
+    <video src={first?.data['480']} poster={first?.preview} controls />
   ) : null;
 };
 
