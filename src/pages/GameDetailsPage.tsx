5691e104bc9283c924ea369ec33c650e6dade787
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-01-28 11:58:59 +0300

    Create your own component for game attributes

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,9 +1,8 @@
+import { Heading, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
-import useGame from '../hooks/useGame';
-import { Heading, Text, Spinner, SimpleGrid } from '@chakra-ui/react';
 import ExpendableText from '../components/ExpendableText';
-import DefinitionItem from '../components/DefinitionItem';
-import CriticScore from '../components/CriticScore';
+import GameAttributes from '../components/GameAttributes';
+import useGame from '../hooks/useGame';
 
 const GameDetailsPage = () => {
   const { slug } = useParams();
@@ -17,26 +16,7 @@ const GameDetailsPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpendableText>{game.description_raw}</ExpendableText>
-      <SimpleGrid columns={2}>
-        <DefinitionItem term="Genres">
-          {game.genres.map(genre => (
-            <Text key={genre.id}>{genre.name}</Text>
-          ))}
-        </DefinitionItem>
-        <DefinitionItem term="Publishers">
-          {game.publishers?.map(publisher => (
-            <Text>{publisher.name}</Text>
-          ))}
-        </DefinitionItem>
-        <DefinitionItem term="Platforms">
-          {game.parent_platforms.map(({ platform }) => (
-            <Text key={platform.id}>{platform.name}</Text>
-          ))}
-        </DefinitionItem>
-        <DefinitionItem term="Metascore">
-          {<CriticScore score={game.metacritic} />}
-        </DefinitionItem>
-      </SimpleGrid>
+      <GameAttributes game={game} />
     </>
   );
 };
